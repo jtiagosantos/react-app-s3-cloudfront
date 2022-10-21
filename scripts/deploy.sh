d2240d0022ab9cd72519b56eaa1653de117ca70c
jtiagosantos <tiagosan040@gmail.com>
2022-10-20 22:02:47 -0300

    chore: implement a script to automatize deploy to aws

---
+++ scripts/deploy.sh
@@ -0,0 +1,19 @@
+USER=$1
+BUCKET_NAME=$2
+DISTRIBUTION_ID=$3
+
+echo "Install dependecies"
+# Install dependencies
+yarn --production
+
+echo "Build app"
+# Build
+yarn build
+
+echo "Sync with S3 Bucket"
+# Sync build with our S3 bucket
+aws s3 sync build s3://$BUCKET_NAME --profile $USER
+
+echo "Invalidate cache"
+# Invalidate cache
+aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --no-cli-pager --profile $USER
