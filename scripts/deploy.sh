069c68d50d6e29320e6e102667bc8b73e16fc788
jtiagosantos <tiagosan040@gmail.com>
2022-10-20 22:30:11 -0300

    chore: update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,10 @@ echo "Build app"
 # Build
 yarn build
 
-echo "Sync with S3 Bucket"
+echo "Sync app with S3 Bucket"
 # Sync build with our S3 bucket
-aws s3 sync build s3://$BUCKET_NAME --profile $USER
+aws s3 sync --delete build s3://$BUCKET_NAME --profile $USER
 
-echo "Invalidate cache"
+echo "Invalidate CloudFront cache"
 # Invalidate cache
 aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*" --no-cli-pager --profile $USER
